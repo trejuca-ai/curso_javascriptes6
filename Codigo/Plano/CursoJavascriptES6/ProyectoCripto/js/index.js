048a239eca22f465690d4f823d7ffc0ba6fa94b6
trejuca-ai <trejuca@outlook.com>
2024-12-10 16:04:23 -0600

    Add files via upload

--- Codigo/Plano/CursoJavascriptES6/ProyectoCripto/js/index.js
+++ Codigo/Plano/CursoJavascriptES6/ProyectoCripto/js/index.js
@@ -5,10 +5,11 @@ const datosMoneda = {
 	moneda:'MXN',
 	criptomoneda: ''
 }
-
+nombres = {}
 /**
  * Constantes para la URL de las APIs
  */
+const URL_IMAGEN = 'https://www.cryptocompare.com'
 const URL_TOP_CRIPTOS = 'https://min-api.cryptocompare.com/data/top/totalvolfull?limit=5&tsym=USD'
 const URL_COTIZACION_CRIPTO = 
 	'https://min-api.cryptocompare.com/data/pricemultifull?fsyms={criptomoneda}&tsyms={moneda}'
@@ -42,25 +43,29 @@ const obtenerCotizacion = (event) => {
 	const urlParseada = URL_COTIZACION_CRIPTO.replace(
 		/{(.*?)}/g, (match, value) => datosMoneda[value]
 	)
-	
+	ejecutarSpinner()
 	fetch(urlParseada)
 		.then(response => response.json())
 		.then(cotizacion => {
 			datosMoneda.criptomoneda.split(',').forEach(cripto => {
-				procesarCotizacion(cotizacion.DISPLAY[cripto][datosMoneda.moneda])
+				console.log(cotizacion)
+				procesarCotizacion(
+					cotizacion.DISPLAY[cripto][datosMoneda.moneda],
+					cripto)
 			})
 		})	
 		.catch(error => console.log(error))
 }
 
-const procesarCotizacion = (cotizacion) => {
+const procesarCotizacion = (cotizacion, criptoName) => {
 	//console.log(cotizacion)
-	const { PRICE, HIGHDAY, LOWDAY, LASTUPDATE, CHANGEPCTDAY } = cotizacion
+	const { PRICE, HIGHDAY, LOWDAY, LASTUPDATE, CHANGEPCTDAY, IMAGEURL } = cotizacion
 	const encabezado = tablaCotizacion.querySelector('thead')
 
 	if (!encabezado) {
 		tablaCotizacion.innerHTML = `
-			<thead>
+			<thead class='encabezado_fila'>
+				<td><b>Imagen</b></td>
 				<td><b>Precio</b></td>
 				<td><b>Precio mas alto</b></td>
 				<td><b>Precio mas bajo</b></td>
@@ -72,6 +77,12 @@ const procesarCotizacion = (cotizacion) => {
 	
 	tablaCotizacion.innerHTML += `
 		<tr>
+			<td>
+				<img 
+					src='${URL_IMAGEN}${IMAGEURL}' 
+					width=30 height=30 
+					title="${nombres[criptoName]}"/>
+			</td>
 			<td>${PRICE}</td>
 			<td>${HIGHDAY}</td>
 			<td>${LOWDAY}</td>
@@ -99,6 +110,7 @@ const cargarCriptos = criptomonedas => {
 		//if (indice === 0) opcion.selected = true
 		
 		criptoMonedaSelector.appendChild(opcion)
+		nombres[Name] = FullName
 	})
 }
 
@@ -110,7 +122,26 @@ const obtenerValorMoneda = (event) => {
 		: selector.value
 		
 	datosMoneda[event.target.name] = tipoMoneda
-	console.log(datosMoneda)
+
 }
 
-
+const ejecutarSpinner = () => {
+	
+	tablaCotizacion.innerHTML = ''
+	
+	const spinner = document.createElement('div')
+	spinner.innerHTML = `
+		<div class="sk-cube-grid">
+		  <div class="sk-cube sk-cube1"></div>
+		  <div class="sk-cube sk-cube2"></div>
+		  <div class="sk-cube sk-cube3"></div>
+		  <div class="sk-cube sk-cube4"></div>
+		  <div class="sk-cube sk-cube5"></div>
+		  <div class="sk-cube sk-cube6"></div>
+		  <div class="sk-cube sk-cube7"></div>
+		  <div class="sk-cube sk-cube8"></div>
+		  <div class="sk-cube sk-cube9"></div>
+		</div>
+	`
+	tablaCotizacion.appendChild(spinner)
+}
